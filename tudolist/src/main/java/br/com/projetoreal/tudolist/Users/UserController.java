9cdd687561a90f1109246bc19a01fe52e09a12e8
bryan-pinaffo <bryan.pinaffo.amp@gmail.com>
2024-09-13 14:30:36 -0300

    commit

--- tudolist/src/main/java/br/com/projetoreal/tudolist/Users/UserController.java
+++ tudolist/src/main/java/br/com/projetoreal/tudolist/Users/UserController.java
@@ -1,12 +1,11 @@
 package br.com.projetoreal.tudolist.Users;
 
 
-import org.apache.catalina.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @Controller
 @RestController
